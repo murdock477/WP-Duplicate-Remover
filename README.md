1718fd4aa9a0189462106a0c0f6d97e77c0b936d
unknown <martin.ilbi@gmail.com>
2017-10-25 20:35:41 +0300

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WP-Duplicate-Remover
 Simple bot with selenium that logs into wordpress admin and deletes duplicate items (requires duplicate remover plugin for wordpress).
+To make sure bot works fine, create a JSON file called 'info' inside your folder and fill this template:
+{
+    "email": "FILLME",
+    "password": "FILLME",
+    "url": "FILLME",
+    "url2": "FILLME"
+}
+
+url being your WP admin login screen and url2 being your duplicate remover plugin url.
---
+++ main.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import json
+from pprint import pprint
+
+browser = webdriver.Chrome("chromedriver.exe")
+
+with open('info.json', 'r') as f:
+    info = json.load(f)
+    email = info['email']
+    password = info['password']
+    url = info['url']
+    url2 = info['url2']
+
+def login():
+    browser.get(url)
+    time.sleep(0.5)
+    browser.find_element_by_id('user_login').send_keys(email)
+    browser.find_element_by_id('user_pass').send_keys(password)
+    browser.find_element_by_id('wp-submit').submit()
+    remover()
+
+def remover():
+    page = 1
+    while page < 96:
+        browser.get(url2)
+        browser.find_element_by_xpath('//*[@id="duplicate_entry_all_apply"]').click()
+        browser.refresh()
+        page += 1
+
+
+login()
